32f6243d177579670a7fd3a2d8733afa7a224372
정다올 <kuma4075@gmail.com>
2024-05-27 17:10:24 +0900

    재 업로드

---
+++ WorkSpace_java/Chap04/src/sec01/exam01/ForExam.java
@@ -0,0 +1,26 @@
+package sec01.exam01;
+
+public class ForExam {
+
+	public static void main(String[] args) {
+		
+		// 1씩 증가하는걸 5번 반복
+		int sum = 0;
+		sum ++;
+		sum ++;
+		sum ++;
+		sum ++;
+		sum ++;
+		System.out.println(sum);
+		
+		// !5 를 할것이다.
+		sum = 0;
+		
+		int n = 1;
+		sum = (n*(n+1) / 2) ;
+		
+		System.out.println(sum);
+		
+	}
+
+}
