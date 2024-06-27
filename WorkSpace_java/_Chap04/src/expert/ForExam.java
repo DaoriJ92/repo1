219fb707b9182a868b04064331ab9e883d451df2
정다올 <kuma4075@gmail.com>
2024-06-27 20:03:03 +0900

    for문 심화

--- WorkSpace_java/_Chap04/src/expert/ForExam.java
+++ WorkSpace_java/_Chap04/src/expert/ForExam.java
@@ -102,12 +102,115 @@ public class ForExam {
         };
         
         
-        
+        System.out.println("=======1111111111===========");
        
+        //3~100 까지 짝수의 갯수를 구하시오.
+        //4,6,8,10......
+        //짝수 = 2로 나눴을때 딱 맞아 떨어지는 수
+        //우선 if문과 System.out.println() 을 활용하여 확인
+        //반복되는 부분 : 반복문으로 넣을 식
+        //
+        int n1 = 0;
+//        n1 = n1 % 2;
+        int count = 0;
+        
+        n1 = 4; // 조건은 3부터이나 3은 나머지가 1이 생겨 반복문이 시작되지 않으므로
+        		// 조건을 만족하는 4를 집어넣어 시작
+        
+        if ((n1 % 2) == 0){
+        	System.out.println(++count);
+        };
+        n1 = 4+2;
+        if ((n1 % 2) == 0){
+        	System.out.println(++count);
+        };
+        n1 = 4+2+2;
+        if ((n1 % 2) == 0){
+        	System.out.println(++count);
+        };
+        n1 = 4+2+2+2;
+        if ((n1 % 2) == 0){
+        	System.out.println(++count);
+        };
+        count = 0;
+        System.out.println("=======1111111111===========");
+        
+        for (n1 = 3 ; n1 <= 100 ; n1++  ) {
+        	if((n1 % 2) == 0) {
+        		count++;
+        	}
+        };System.out.println(count);
+        
+        i = 0;
+        int j = 0;
+        int k = 0;
+        // 구구단 2단을 for문을 활용하여 구현하시오.
+        // 구구단 2단의 구조 파악
+        // 2 x 1 = 2
+        // 2 x 2 = 4
+        // 2 x 3 = 6
+        // 2 x 4 = 8
+        // 2 x 5 = 10
+        // 2 x 6 = 12
+        // 2 x 7 = 14
+        // 2 x 8 = 16
+        // 2 x 9 = 18
+        // i   j  각각 변수로 잡으면
+        // 2에 1~9 까지 곱한 값을 구하는게 구구단 2단
+        System.out.println("=======333=========");
+        if (j<=9) {
+        	j++; System.out.println("2x"+j+"=" + (2*j));
+        };
+        //
+        //(j >= 1 && j<= 9);
+        // j는 한번 반복 될때마다 1씩 늘어난다
+        // j++
+//        i = 2;
+        System.out.println("=======333=========");
+        for(j = 1; j >=1 ; j++) {if(j <=9) {
+        	System.out.println("2x"+j+"=" + (2*j));};
+        };
+        System.out.println("=======444=========");
+        //총 체력이 100일때
+        //데미지가 17인 무기로 몇번 맞아야 죽을까?
+        //L = 100, D = 17
+        //
+        //L - D 의 값이 0보다 작아질때 까지 반복
+        int L = 100; //life
+        int D = 17;	 //damage
+//        boolean t = L < 0;
       
+        System.out.println(L); // 출력값 = 100
         
+        if(L>0) {L =L-D;}
+        System.out.println(L); // 출력값 = 83 
+       
+        if(L>0) {L =L-D;}
+        System.out.println(L); // 출력값 = 66
         
+        if(L>0) {L =L-D;}
+        System.out.println(L); // 출력값 = 49
         
-     
+        if(L>0) {L =L-D;}
+        System.out.println(L); // 출력값 = 32
+       
+        	if(L>0) {L =L-D;} 
+        	System.out.println(L);
+        	System.out.println("=======555=========");
+     int hit = 0;
+        for(L=100 ; L > 0 ;L = L - D  ){
+        	hit++; 
+        };
+        System.out.println(hit);
+        
+        // 일주일에 6만원 범
+        // 투자금이 400만원일때
+        // 원금 회복까진 총 몇주가 걸릴까.     
+         
+        int seed = 4000000;
+        int w = 60000;
+        //우선 10000으로 나누어 숫자를 간소화 한다.
+        seed = 400;
+        w = 6;
     }
 }
